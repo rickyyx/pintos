8fbef641a7eaad40d038438210a1dc77893dd1b3
Ricky Chen Xu <chen11@corn12.stanford.edu>
2017-05-21 21:42:18 -0700

    Start P1: timer

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -30,6 +30,7 @@ static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 static void real_time_delay (int64_t num, int32_t denom);
 
+
 /* Sets up the timer to interrupt TIMER_FREQ times per second,
    and registers the corresponding interrupt. */
 void
