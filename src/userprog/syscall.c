6d7d6d81bd70e0b6403f6e5228b993f276a4a09b
githubxxcc <xuchen@u.nus.edu>
2017-10-25 14:55:02 -0700

    Done reading arguments

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -9,16 +9,18 @@
 
 /* Syscalls */
 static void syscall_handler (struct intr_frame *);
-static void _exit(const int);
-
+static void syscall_exit(int*, struct intr_frame*);
 
 /* Utility methods */
 static bool valid_syscall_num(const int);
-static int get_syscall_argv(int, int*);
+static int get_syscall_argv(int, int*, int*);
+static bool valid_user_vaddr(void*);
 
+static void _exit(const int);
 
 typedef void (*syscall_func) (int*, struct intr_frame*);
 static syscall_func syscall_table[SYS_NUM];
+static int syscall_argc_table[SYS_NUM];
 
 
 
@@ -26,22 +28,27 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+
+  //_exit
+  syscall_table[SYS_EXIT] = syscall_exit;
+  syscall_argc_table[SYS_EXIT] = 1;
+
 }
 
 static void
 syscall_handler (struct intr_frame *f) 
 {
   int syscall_num, * argv;
-  void * esp = f->esp;
+  int * esp = f->esp;
 
-  if(!is_user_vaddr(esp) && valid_syscall_num(*(int*)esp)){
+  if(!is_user_vaddr(esp) && valid_syscall_num(*esp)){
       _exit(-1);
   }
 
-  syscall_num = *(int*)esp;
+  syscall_num = *esp++;
   argv = malloc(4 * sizeof(int));
 
-  if(!get_syscall_argv(syscall_num, argv)) {
+  if(!get_syscall_argv(syscall_num, argv, esp)) {
       free(argv);
       _exit(-1);
   }
@@ -58,13 +65,34 @@ valid_syscall_num(const int num)
 
 
 static int
-get_syscall_argv(const int syscall_num UNUSED, int * argv UNUSED) 
+get_syscall_argv(const int syscall_num, int * argv, int* esp) 
 {
-    return 1;
+    int syscall_argc = syscall_argc_table[syscall_num];
+
+    while(syscall_argc--){
+        if(!valid_user_vaddr(esp))
+            return -1;
+        *argv++ = *esp++;
+    }
+    return 0;
+}
+
+
+static bool 
+valid_user_vaddr(void * addr)
+{
+    //TODO: Check if the page is mapped 
+    return addr != NULL && is_user_vaddr(addr);
 }
 
 static void
 _exit(int status UNUSED)
+{
+
+}
+
+static void
+sys_call(int* argv, struct intr_frame * cf)
 {
 
 }
