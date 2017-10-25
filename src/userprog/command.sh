a63b3d86ad420a9c1335068a419bf581235b3acb
githubxxcc <xuchen@u.nus.edu>
2017-10-25 15:47:39 -0700

    Syscall: exit

--- src/userprog/command.sh
+++ src/userprog/command.sh
@@ -1,8 +1,10 @@
 #pintos --filesys-size=2 -p ../../examples/pwd -a pwd -- -f -q run 'pwd'
 #pintos --gdb  --filesys-size=2 -p tests/userprog/args-many -a args-many -- -f run args-many
 #pintos --gdb  --filesys-size=2 -p tests/userprog/args-many -a args-many -- -f run args-many
+cd build/ &&
+  # make tests/userprog/exit.result VERBOSE=1 
 
-
+pintos --gdb  --filesys-size=2 -p tests/userprog/exit -a exit -- -f run exit
 # pintos --gdb  --filesys-size=2 -p ../../examples/echo -a echo -- -f run 'echo hello here'
 
-pintos --gdb  --filesys-size=2 -p ../../examples/ls -a '/bin/ls' -- -f run '/bin/ls -l foo bar'
+#pintos --gdb  --filesys-size=2 -p ../../examples/ls -a '/bin/ls' -- -f run '/bin/ls -l foo bar'
